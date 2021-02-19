4f58ab05af068c528d21d6f822fdd6beaa577ba3
Miriam <miriam.haenle@gmail.com>
2021-02-19 14:55:15 +0100

    Update headline

--- src/Headline.js
+++ src/Headline.js
@@ -1,5 +1,5 @@
 import './Headline.css';
 
-export default function Headline({ name }) {
-  return <h2 className="headline">{name}</h2>;
+export default function Headline({ title }) {
+  return <h2 className="headline">{title}</h2>;
 }
